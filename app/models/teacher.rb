952a8e760330690387237b7eda0b5542be53a128
Cathella <ncathie214@gmail.com>
2021-01-06 00:41:26 +0300

    add profile image to teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,6 +4,8 @@ class Teacher < ApplicationRecord
   validates_presence_of :phone, :name
   validates_uniqueness_of :phone
 
+  has_one_attached :image, :dependent => :destroy
+
   belongs_to :group, optional: true
   has_one :direction, :dependent => :destroy
 end
