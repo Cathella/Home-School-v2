5f8b4277239f3e9b7d8f8c462db8767a8dc72c66
Cathella <ncathie214@gmail.com>
2021-01-29 16:28:26 +0300

    validate teacher form fields

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,20 +2,15 @@ class Teacher < ApplicationRecord
   has_secure_password
 
   validates_presence_of :phone, :name
+
+  validates :name, :length => { :minimum => 3, :maximum => 80 }
+  validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
+
   validates_uniqueness_of :phone
+  validates :phone, :numericality => true, :length => { :minimum => 10, :maximum => 12 }
 
   has_one_attached :image, :dependent => :destroy
 
   belongs_to :group, optional: true
   has_one :direction, :dependent => :destroy
-
-  # validates :name, :presence => true, :length => { :minimum => 3, :maximum => 80 }
-  # validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
-  # validates_uniqueness_of :phone
-  # validates :phone, :presence => true, :numericality => true, :length => { :minimum => 10, :maximum => 12 }
-
-  # has_one_attached :image, :dependent => :destroy
-
-  # belongs_to :group, optional: true
-  # has_one :direction, :dependent => :destroy
 end
