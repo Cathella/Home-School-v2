5eb14b8f61790267d4afc29fcb0c046ff8548a46
Cathella <ncathie214@gmail.com>
2021-01-29 15:55:19 +0300

    revert child model

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,10 +1,20 @@
 class Child < ApplicationRecord
+  # has_secure_password
+
+  # validates :name, :presence => true, :length => { :minimum => 3, :maximum => 80 }
+  # validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
+  # validates_uniqueness_of :phone
+  # validates :phone, :presence => true, :numericality => true, :length => { :minimum => 10, :maximum => 12 }
+
+  # has_one_attached :image, :dependent => :destroy
+  
+  # belongs_to :group, optional: true
+  # has_one :address, :dependent => :destroy
+
   has_secure_password
 
-  validates :name, :presence => true, :length => { :minimum => 3, :maximum => 80 }
-  validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
+  validates_presence_of :phone, :name
   validates_uniqueness_of :phone
-  validates :phone, :presence => true, :numericality => true, :length => { :minimum => 10, :maximum => 12 }
 
   has_one_attached :image, :dependent => :destroy
   
