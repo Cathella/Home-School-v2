3c2611fca0be702115f77a777e0c41f6ca189493
Cathella <ncathie214@gmail.com>
2021-02-14 11:30:48 +0300

    add admin email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,6 @@ class AdminMailer < ApplicationMailer
   default from: 'admin@homeschool.ug'
 
   def account_created_email
-    mail(to: 'ncathie214@gmail.com', subject: 'A new user has signed up!')
+    mail(to: 'homeschoolug@gmail.com', subject: 'A new user has signed up!')
   end
 end
