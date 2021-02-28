0eb05099ec7db332c96af8c7e53222fb57fbf24a
Cathella <ncathie214@gmail.com>
2021-02-28 10:18:55 +0300

    style profiles show and  index

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: %i[ show edit update destroy ]
+  before_action :teacher_authorize, only: [:new, :edit, :update, :show]
 
   # GET /profiles or /profiles.json
   def index
