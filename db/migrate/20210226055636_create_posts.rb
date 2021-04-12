68dca3ad3bae2e25637e24804d132dd003a1d40b
Cathella <ncathie214@gmail.com>
2021-04-12 23:58:48 +0300

    add action text styles

--- db/migrate/20210226055636_create_posts.rb
+++ db/migrate/20210226055636_create_posts.rb
@@ -1,7 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.text :description
       t.string :subject
       t.string :grade
       t.string :meeting
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_125459) do
+ActiveRecord::Schema.define(version: 2021_04_12_203526) do
+
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -175,7 +185,6 @@ ActiveRecord::Schema.define(version: 2021_04_12_125459) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.text "description"
     t.string "subject"
     t.string "grade"
     t.string "meeting"
@@ -195,7 +204,6 @@ ActiveRecord::Schema.define(version: 2021_04_12_125459) do
     t.integer "fee"
     t.integer "experience"
     t.string "commitment"
-    t.text "description"
     t.integer "teacher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -232,4 +240,6 @@ ActiveRecord::Schema.define(version: 2021_04_12_125459) do
   end
 
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "children"
+  add_foreign_key "profiles", "teachers"
 end
