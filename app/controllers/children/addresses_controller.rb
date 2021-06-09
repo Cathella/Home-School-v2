6aff914e387555ad26f526f04a35fc69265bcdb5
Cathella <ncathie214@gmail.com>
2021-06-10 01:50:51 +0300

    setting up address

--- app/controllers/children/addresses_controller.rb
+++ app/controllers/children/addresses_controller.rb
@@ -1,19 +1,39 @@
 class Children::AddressesController < ApplicationController
   before_action :set_child
   before_action :set_address, only: [:edit, :update]
-  
+
   def new
-    
+    @address = Address.new
   end
 
   def create
-    
+    @address = @child.address.create address_params
+    @address.child_id = current_child.id
+
+    if @address.save
+      redirect_to dashbord_path, notice: "Your Address has been saved!"
+    else
+      redirect_to new_child_address_path, notice: "Failed to save Address!"
+    end
   end
   
   def edit  
   end
   
   def update
-    
   end
+
+  private
+
+    def set_address
+      @address = Address.find(params[:id])
+    end
+
+    def set_child
+      @child = Child.find(params[:child_id])
+    end
+
+    def address_params
+      params.require(:address).permit(:landmark, :longitude, :latitude)
+    end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :child
+  validates :latitude, :longitude, :landmark, presence: true
 end
