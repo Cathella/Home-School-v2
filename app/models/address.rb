8d574690a2c0e8e56c488910869281e877ef2108
Cathella <ncathie214@gmail.com>
2021-06-09 18:46:23 +0300

    redefine dashbord features

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,5 @@
 class Address < ApplicationRecord
-  extend FriendlyId
-  friendly_id :landmark, use: :slugged
-
   validates :latitude, :longitude, :landmark, presence: true
-
   belongs_to :child, optional: true
-
   scope :get_lat_lon_array, -> { pluck(:latitude, :longitude ) }
 end
- 
--- app/models/direction.rb
+++ app/models/direction.rb
@@ -1,10 +1,5 @@
 class Direction < ApplicationRecord
-  extend FriendlyId
-  friendly_id :landmark, use: :slugged
-  
   validates :latitude, :longitude, :landmark, presence: true
-
   belongs_to :teacher, optional: true
-
   scope :get_lat_lon_array, -> { pluck(:latitude, :longitude ) }
 end
