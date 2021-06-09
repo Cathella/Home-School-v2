414fff290d10ae7e1e7c75b42b86a55d8eb6bad3
Cathella <ncathie214@gmail.com>
2021-06-09 15:29:08 +0300

    various updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+    end
+    
 end
