eeca6f72218fd70bbf7c42509efba3900018cb3c
Cathella <ncathie214@gmail.com>
2021-06-10 19:10:21 +0300

    add show, index action and views to group

--- app/controllers/teachers/groups_controller.rb
+++ app/controllers/teachers/groups_controller.rb
@@ -1,14 +1,29 @@
 class Teachers::GroupsController < ApplicationController
   before_action :set_teacher
-  before_action :set_group, only: [ :edit, :update ]
+  before_action :set_group, only: [ :edit, :update, :show ]
+
+  def index
+    @groups = current_teacher.groups
+  end
 
   def new
     @group = Group.new
+    # @value = 5.times.map { (0...(rand(10))).map { ('a'..'z').to_a[rand(26)] }.join }.join(" ")
+    @group_name = current_teacher.full_name.parameterize(separator: '-') + "-study-group"
+  end
+
+  def show
   end
 
   def create
     @group = @teacher.groups.new group_params
     @group.teacher_id = current_teacher.id
+
+    if @group.save
+      redirect_to @group, notice: 'Group was successfully created.'
+    else
+      redirect_to new_teacher_group_path, notice: "Group not created. Try Again!"
+    end
   end
 
   private
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :teacher
   validates :name, :subject, presence: true
+  validates_uniqueness_of :name
 end
