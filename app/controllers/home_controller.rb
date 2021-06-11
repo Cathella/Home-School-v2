fcd9a4abbc31913bf68544a2ee1ed0c2ab73842e
Cathella <ncathie214@gmail.com>
2021-06-11 07:38:41 +0300

    various deletions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,30 +5,6 @@ class HomeController < ApplicationController
   def dashboard
   end
 
-  def myaddress
-    @address = current_child.address
+  def account
   end
-
-  def teacheraddress
-    @direction = current_teacher.direction
-  end
-
-  def mygroup
-    @group = current_child.group
-  end
-
-  def teachergroup
-    @group = current_teacher.group
-  end
-
-  def joined
-  end
-
-  def lessons
-  end
-
-  def privateaching
-  end
-
-  def account; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :children
   devise_for :teachers
-  # resources :profiles, :posts, :videos, :groups, :conversations, :messages
-  # resources :child_sessions, :teacher_sessions, :admins, :admin_sessions
-  # resources :contacts, :teacher_password_resets, :password_resets, :directions, :addresses
+  # resources :conversations, :messages
 
   root to: 'home#index'
   
@@ -20,18 +18,8 @@ Rails.application.routes.draw do
 
   resources :groups
 
-  # get "/teachershome", to: "teachers#teachershome", as: :teachershome 
-  # get "lessons", to: "home#lessons", as: "lessons"
-  # get "joined", to: "home#joined", as: "joined"
-  # get "privateaching", to: "home#privateaching", as: "privateaching"
-  # get "mygroup", to: "home#mygroup", as: "my_group"
-  # get "teachergroup", to: "home#teachergroup", as: "teacher_group"
-  # get "/childrenhome", to: "children#childrenhome", as: :childrenhome
-  # get "myaddress", to: "home#myaddress", as: "my_address"
-  # get "teacheraddress", to: "home#teacheraddress", as: "teacher_address"
   get "accounts", to: "home#account", as: "accounts"
   get "dashboard", to: "home#dashboard", as: "dashboard"
-  # get 'messages/create'
   
   patch :to_trash, to: 'conversations#to_trash', as: :to_trash
   patch :from_trash, to: 'conversations#from_trash', as: :from_trash
