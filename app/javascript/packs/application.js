04195d6cfd76e929dba2fd084011e7a20153e137
Cathella <ncathie214@gmail.com>
2021-06-29 09:04:56 +0300

    more video updates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,13 @@
+//= require local-time
+
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
 import "controllers";
+import LocalTime from "local-time";
+
+Rails.start();
+Turbolinks.start();
+LocalTime.start();
 
 window.initMap = function (...args) {
   const event = document.createEvent("Events");
